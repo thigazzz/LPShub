47bc37c656dd013acd7f59d6ca22684ab5a21da3
thigazzz <thiago.p.dasilva2005@gmail.com>
2024-03-06 15:27:27 -0300

    refactor: run a broken file + add: test when path not exists

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,6 +3,7 @@ Test run script feature
 """
 import os
 import subprocess
+import re
 from pytest import mark
 from typing import Tuple
 
@@ -12,9 +13,13 @@ def is_not_path_correct(path: str) -> bool:
     return False
 
 def run_script(path_file: str) -> Tuple[int, str]:
-    result = subprocess.run(['python3', path_file], stdout=subprocess.PIPE)
-    if result.returncode > 0:
+    if os.path.isfile(path_file) == False:
         return (1, "Broken")
+
+    result = subprocess.run(['python3', path_file], stdout=subprocess.PIPE)
+
+    if result.returncode > 0:
+        return (1, "Error running this file")
     # os.system(f"source venv/bin/activate")
     # os.system(f"python3 {path_file}")
     # code = process.exitcode
@@ -35,6 +40,18 @@ def test_show_error_when_file_not_have_success_running():
     
     """
     sut = run_script
-    error = sut('broken_path')
+    fake_path = './tests/fake/fake_broken_file.py'
+    with open(fake_path, 'a') as f:
+        f.write("raise Exception('Error')")
+
+    error = sut(fake_path)
+
+    assert error[0] > 0
+    assert error[1] == "Error running this file"
+    os.remove(fake_path)
+
+def test_show_error_when_path_is_broken():
+    sut = run_script
+    error = sut('broken_path/broken_file.any')
     assert error[0] > 0
     assert error[1] == "Broken"
