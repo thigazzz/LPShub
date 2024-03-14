39536bb8ab99b855ff4b6c94d1b1f4cefee484f1
thigazzz <thiago.p.dasilva2005@gmail.com>
2024-03-14 16:14:36 -0300

    refactor: delete not required function

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,6 +3,7 @@ Test for running a script.
 """
 import os
 from lpshub.run_script import run_script
+from lpshub.crud import Script
 
 def test_show_error_when_file_not_have_success_running():
     """
@@ -27,30 +28,11 @@ def test_show_error_when_file_not_have_success_running():
     fake_path = './tests/fake/fake_broken_file.py'
     with open(fake_path, 'a') as f:
         f.write("raise Exception('Error')")
-    
+    s = Script(1, fake_path, False)
 
-    error = sut(fake_path)
+    error = sut(s)
 
     assert error[0] > 0
     assert error[1] == "Error running this file"
 
     os.remove(fake_path)
-
-def test_show_error_when_path_is_broken():
-    """   
-    Test for when running a wrong path or named Python file.
-
-    An example of how the API of this behavior must be:
-    >>> run_script(wrong_name)
-    (1, 'Error') == (exit_code, error's message)
-
-    >>> run_script(nonexistent_file)
-    (1, 'Error') == (exit_code, error's message)
-
-    >>> run_script(wrong_path_file)
-    (1, 'Error') == (exit_code, error's message)
-    """
-    sut = run_script
-    error = sut('broken_path/broken_file.any')
-    assert error[0] > 0
-    assert error[1] == "Broken"
